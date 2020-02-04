2dd5a807d838e22357190d35d4d7bcd1b0e1005e
the-provost <mailstojesil@gmail.com>
2020-02-04 16:40:42 +0300

    AdminLTE+Vue+Auth+VueKanban Installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Achiever 👍
                 </div>
-
+                    <h2>A goal planner for everyone.</h2>
+                    <br>
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/the-provost/achiever">GitHub</a>
+                    <a href="http://jesilsnotes.com">Blog</a>
+                    <a href="http://greymore.tech">Greymore Tech</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
